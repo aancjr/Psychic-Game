67cd2908b8667b51a1ef9e1fc8ae2b4bc6c3b2b3
aancjr <45945137+aancjr@users.noreply.github.com>
2019-04-08 18:34:32 -0400

    console logging to check flow. running into syntax error with the {} in the if/else statements. working on this.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,71 @@
-var wins = 0;
-var losses = 0;
-var guesses = 9;
-var guessChoices = [];
 
-document.onkeyup = function(event) {
-    console.log(event.key);
-
+    //Array for listing available game key-stroke options. In this case, all letters.
     var computerChoices = [];
 
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    //Variabls defined and where they start.
+    var wins = 0;
+    var losses = 0;
+    var guesses = 9;
+    var guessChoices = [];
+    
+    //Variabls holding references for HTML use. 
 
-    var playerGuess = event.key;
+    var instructionsText = document.getElementById("instructionsText");
+    var computerChoiceText = document.getElementById("computerChoiceText");
+    var winsText = document.getElementById("winsText");
+    var lossesText = document.getElementById("lossesText");
+    var guessesLeftText = document.getElementById("guessesLeftText");
 
-    if (playerGuess === computerGuess) {
-        wins++;
-        guesses = 9;
-        guessChoices = [];
+    //Function for when users presses key.
 
-        console.log(playerGuess)
+    document.onkeyup = function(event) {
+
+        //Which key is pressed.        
+        var playerGuess = event.key;
+
+        //The computers choice.        
+        var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    
+        //Determining the outcome of the game.
+        if (playerGuess === computerGuess) {
+            wins++;
+            guesses = 9;
+            guessChoices = [];
+    
+            console.log(playerGuess);
+        }
+    
+        if (playerGuess != computerGuess) {
+            guesses--;
+        }
+    
+        if (playerGuess = 0) {
+            losses++;
+            guesses = 9;
+            guessChoices = [];
+        }
+
+        //Display user guesses, wins, losses and ties.
+        userChoiceText.textContent = "Letters guessed: " + playerGuess;
+        winsText.textContent = "Wins: " + wins;
+        lossesText.textContent = "Losses: " + losses;
+
+
     }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
