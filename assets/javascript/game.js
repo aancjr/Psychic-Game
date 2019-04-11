5af62dee8c0af64a833f9a2c31569b3d5c2cdda8
aancjr <45945137+aancjr@users.noreply.github.com>
2019-04-11 18:32:22 -0400

    got the if statements to work, game function is there with the array of letters populating. Next: need letters guessed and computer guess to reset at the end of the game. git status

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,18 +7,11 @@
     var losses = 0;
     var guesses = 9;
     var guessedLetters = [];
-    var guessesLeft = guesses - 1;
     
-    /*Variabls holding references for HTML use. Had this going before i used the document.getElementById below
-
-    var instructionsText = document.getElementById("instructionsText");
-    var computerChoiceText = document.getElementById("computerChoiceText");
-    var winsText = document.getElementById("winsText");
-    var lossesText = document.getElementById("lossesText");
-    var guessesLeftText = document.getElementById("guessesLeftText");*/
-
-
-
+    //The computers choice.        
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        
+    console.log(computerGuess);
     //Function for when users presses key.
      document.onkeyup = function(event) {
 
@@ -28,45 +21,34 @@
         
         console.log(playerGuess);
         
-        //The computers choice.        
-        var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-        
-        console.log(computerGuess);
        
         //Determining the outcome of the game.
 
-        //If player guesses the correct letter, wins increase. Guesses restart with 9.
-        if (playerGuess === letterIndex) {
-        
             if (playerGuess === computerGuess) {
                 wins++;
                 guesses = 9;
+                guessedLetters = [];
+    
+                console.log(playerGuess);
             }
-        
-            //If player guess does not match the computer guess, subtract decrementally with each incorrect guess.
-            else if ((playerGuess != computerGuess) && (guesses >1)) {
-                guessesleft;
+    
+            if (playerGuess != computerGuess) {
+                guesses--;
+                guessedLetters = [];
             }
-
-            //If player guesses are less than 1, game over and players losses increase. Guesses restart to 9.   
-            else { 
+    
+            if (guesses === 0) {
                 losses++;
                 guesses = 9;
                 guessedLetters = [];
-            }
         }
             
         //Display user guesses, wins, losses and ties.
-        document.getElementById("userChoiceText").textContent = "Letter's Guessed: " + playerGuess;
+        document.getElementById("userChoiceText").textContent += playerGuess + ", " ;
         document.getElementById("computerChoiceText").textContent = "Computer's Choice: " + computerGuess;
         document.getElementById("winsText").textContent = "Wins: " + wins;
         document.getElementById("lossesText").textContent = "Losses: " + losses;
-        document.getElementById("guessesLeftText").textContent = "Guesses Left: " + guessesLeft;
-        
-        /*winsText.textContent = "Wins: " + wins;
-        lossesText.textContent = "Losses: " + losses;
-        guessesLeftText.textContent = "Guesses Left:" + guessedLetters;
-        computerChoiceText.textContent = "Computer's Choice" + computerGuess;*/
+        document.getElementById("guessesLeftText").textContent = "Guesses Left: " + guesses;
         
         }
 
